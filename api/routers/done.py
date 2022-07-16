fe562d01bf28fa8d7f2b7c4ccb95a1d7665f220b
kimura572 <kimurafumiya7020@icloud.com>
2022-07-16 22:21:21 +0900

    shemaデータの受け渡し20220716

--- api/routers/done.py
+++ api/routers/done.py
@@ -8,11 +8,11 @@ router = APIRouter()
 
 
 
-@router.put("tasks/{task_id}/done")
-async def mark_task_as_done():
-  pass
+@router.put("tasks/{task_id}/done", response_model=None)
+async def mark_task_as_done(task_id: int):
+  return
 
 
-@router.delete("tasks/{task_id}/done")
-async def unmark_task_as_done():
-  pass
+@router.delete("tasks/{task_id}/done", response_model=None)
+async def unmark_task_as_done(task_id: int):
+  return
